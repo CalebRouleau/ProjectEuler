ef3f4ce83ba8557b8331e576975c93293574a9d4
CalebRouleau <calebrouleau@gmail.com>
2012-12-24 13:13:28 -0500

    added test cases and fixed bug of getPrimes not returning the maxP if maxP is a prime

--- Primes.py
+++ Primes.py
@@ -1,7 +1,11 @@
 import math
+import sys
 
 #credit for ideas goes to holmezideas.com
+#returns True is the input is a prime number
+#and false otherwise
 def isPrime(num): 
+	assert num == int(num)
 	num = int(num);
 	if(num<=1): 
 		return False
@@ -20,16 +24,17 @@ def isPrime(num):
 	return True
 
 
-
+#maxP is the greatest prime that this function
+#will generate
 def getPrimes(maxP): 
 	primes = [];
 	allnums = [];
-	for num in range(0, maxP): 
+	for num in range(0, maxP+1): 
 		allnums.append(1);
 		#allnums[num] = 1;
 
 	i = 2;
-	while(i<maxP): 
+	while(i<=maxP): 
 		if allnums[i] == -1: 
 			i += 1;
 			continue;
@@ -37,10 +42,20 @@ def getPrimes(maxP):
 		primes.append(i);
 		multis = i*i; 
 
-		while(multis < maxP): 
+		while(multis <= maxP): 
 			allnums[multis] = -1;
 			multis += i
 
 		i += 1;
 
 	return primes;
+
+
+if __name__ == "__main__": 
+	if(len(sys.argv) > 1 and sys.argv[1] == "test"): 
+		assert isPrime(13)
+		assert not isPrime(14)
+
+		assert getPrimes(7) == [2, 3, 5, 7]
+
+
