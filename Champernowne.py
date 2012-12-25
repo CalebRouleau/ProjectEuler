670b96e0ff486cb4f96238c9c256a516bff2248f
CalebRouleau <calebrouleau@gmail.com>
2012-12-25 03:58:37 -0500

    solved Q40

--- Champernowne.py
+++ Champernowne.py
@@ -1,31 +1,91 @@
 import math
 import sys
+import DoubleBase
+import Truncatable
 
 lengthCount = [0, 9, 100, 1000, 10000, 100000, 1000000]
 
 def getNumDigits(num): 
 	return int(math.log10(num))+1;
 
-def digitSearch(digit): 
-	lengthRange = 0;
-	for length in range(1, 6): 
-		newD = digit - length*lengthCount[length]
-		if newD < 0: 
-			lengthRange = length
-			break
+def getDigits(digit): 
+	inputDigit = digit
+
+	digit -= 1
+
+	count = 1
+	magnitude = 0
+	
+	finished = False
+	if(not finished): 
+		if(digit - 9 > 0): 
+			digit -= 9
+			count += 9
 		else: 
-			digit = newD
-	
-	#find where digit is in lengthRange
-	
+			finished = True
+			magnitude = 1
+
+	if(not finished): 
+		if(digit - 90*2 > 0): 
+			digit -= 90*2
+			count += 90
+		else: 
+			finished = True
+			magnitude = 2
+
+	if(not finished): 
+		if(digit - 900*3 > 0): 
+			digit -= 900*3
+			count += 900
+		else: 
+			finished = True
+			magnitude = 3
+
+	if(not finished): 
+		if(digit - 9000*4 > 0): 
+			digit -= 9000*4
+			count += 9000
+		else: 
+			finished = True
+			magnitude = 4
+
+	if(not finished): 
+		if(digit - 90000*5 > 0): 
+			digit -= 90000*5
+			count += 90000
+		else: 
+			finished = True
+			magnitude = 5
+
+	if(not finished): 
+		if(digit - 900000*6 > 0): 
+			digit -= 900000*6
+			count += 900000
+		else: 
+			finished = True
+			magnitude = 6
+
+	assert finished
+	count += int(digit/magnitude)
+	subDigit = digit%magnitude
+	print "input digit is", inputDigit
+	print "digit is",digit
+	print "magnitude is",magnitude
+	print "count is",count
+	print "subDigit is", subDigit
+	countOrder = getNumDigits(count)
+	print "countOrder is", countOrder
+	returnValue = DoubleBase.getIndex10(count, countOrder -subDigit-1)
+	print "return value is",returnValue
+	return returnValue
+		
 
 def main(): 
 	expr = 1; 
 	for exp in range(7): 
 		digit = int(math.pow(10,exp))
-		print digit; 
-
-	
+		expr *= getDigits(digit)
+	print expr
 
 
 if __name__ == "__main__": 
@@ -35,5 +95,16 @@ if __name__ == "__main__":
 		assert getNumDigits(5) == 1
 		assert getNumDigits(2345) == 4
 		assert getNumDigits(1) == 1
+
+
+		assert getDigits(4) == 4
+		assert getDigits(14) == 1
+		assert getDigits(15) == 2
+		assert getDigits(16) == 1
+		assert getDigits(17) == 3
+		assert getDigits(18) == 1
+		assert getDigits(19) == 4
+
+
 	else: 
 		main()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Solutions to projecteuler.net problems with some focus on creating reusable code
 all files should be testable by running the command "python filename.py test"
 
 Q36 is solved by DoubleBase.py
-
 Q37 is solved by Truncatable.py
-
 Q38 is solved by Pandigital.py
+Q39 is solved by Triangle.py
+Q40 is solved by Champernowne.py
